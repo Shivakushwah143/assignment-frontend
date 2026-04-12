21972bdb0e697c9effcba2bd47888afb44e6720e
shiva kushwah <shivakushwah144@gmail.com>
2026-04-12 16:17:12 +0530

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  base: '/assignment-frontend/', // Your repository name here
 })
